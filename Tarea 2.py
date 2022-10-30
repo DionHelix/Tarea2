25eb0f64c6b97da68ca39f22ab2401e446d55e3b
DionHelix <dionhelix@gmail.com>
2022-10-29 21:04:28 -0500

    Explorando otro optimizador, sin gran cambio

--- Tarea 2.py
+++ Tarea 2.py
@@ -58,14 +58,16 @@ input_shape=(192, 192, 3)
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape, data_format='channels_last'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Flatten())
 model.add(layers.Dense(40))
 model.summary()
 
-model.compile(optimizer='adam',
+model.compile(optimizer='RMSprop',
               loss=tf.keras.losses.CategoricalCrossentropy(reduction=tf.keras.losses.Reduction.NONE),
               metrics=['accuracy'])
 history = model.fit(train_data, epochs=10, 
